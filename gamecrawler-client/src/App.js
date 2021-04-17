c52d72c6bbb43088235a1d1e134b6f43cfeea89e
Seul An Kim <sakim5027@gmail.com>
2021-04-17 00:57:12 -0500

    feature 2/로그인 dialog / created login dialog

--- gamecrawler-client/src/App.js
+++ gamecrawler-client/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import { Container } from './Container';
+
 
 function App() {
+  const triggerText = 'Login';
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+  };
   return (
     <div>
       <h1>Hello World!</h1>
       <h2>Welcome to Game Crawler!</h2>
+      <Container triggerText={triggerText} onSubmit={onSubmit} />
     </div>
   );
 }
