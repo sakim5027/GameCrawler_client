4a0551ed1f1fab36e0f7b8d15ecf5dc6fdf8b547
Hyeonyeong Kim <hyeonyeongkim@Hyeonyeongui-MacBookPro.local>
2021-04-23 15:06:17 +0900

    feature6 game api modified but has an error

--- gamecrawler-client/src/pages/Home/homeGameSearch.js
+++ gamecrawler-client/src/pages/Home/homeGameSearch.js
@@ -4,10 +4,20 @@ import { fakeData } from './fakeData';
 import Top5Games from './top5Gmaes'
 
 function HomeGameSearch() {
+    const [game, setGame] = useState([]);
     const [filteredEl, setFilteredEl] = useState('');   //필터링된 게임을 저장
     const genreEl=["Point-and-click","Fighting","Shooter","Music"];         //나중에 장르 db도 있는지 봐야지
     const genreOption= genreEl.map(el =>{return <option value={el}>{el}</option>});
-
+        useEffect( ()=>{
+            axios
+                .get('http://ec2-18-189-171-239.us-east-2.compute.amazonaws.com:5000/games',    // 게임 정보를 여기서 가져오는 게 맞나? 배치 서버? https://api.igdb.com/v4/games
+                // .get('https://api.igdb.com/v4/games',
+                {withCredentials:true}
+                ).then(res =>{
+                    console.log(res.data)
+                    setGame(res.data)
+                }).catch(err =>alert(err))
+        }, [])
         return (
             
             <div className= "gameSearch">
@@ -20,9 +30,13 @@ function HomeGameSearch() {
                         {genreOption}
                     </select>
                     <input type="text" placeholder="Search" onChange={e=>{setFilteredEl(e.target.value)}}/>
+                    {/* <input type="text" placeholder="Search"/>
+                    <button className = "submitBtn" onClick={e=>{setFilteredEl(e.target.value)}}>조회</button> */}
+                    
                 </div>
                 <div className="filteredGames">
-                {fakeData.filter(el =>{
+                {/* 나중엔 fakeData가 아니라 game으로 수정해야 함 */}
+                {fakeData.filter(el =>{ 
                     if(filteredEl===""){
                         return el   
                     }else if((el.title.toLowerCase().includes(filteredEl.toLocaleLowerCase()))||(el.genre.toLowerCase().includes(filteredEl.toLocaleLowerCase()))){
@@ -44,3 +58,5 @@ function HomeGameSearch() {
 
 
 export default HomeGameSearch;
+
+
