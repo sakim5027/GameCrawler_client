63ed63e901d7f4703fa92440ae6ad7b456aa92e6
Hyeonyeong Kim <hyeonyeongkim@Hyeonyeongui-MacBookPro.local>
2021-04-23 12:34:27 +0900

    feature6 home gameSearch api updated and feature15 css modified.

--- gamecrawler-client/src/App.js
+++ gamecrawler-client/src/App.js
@@ -10,6 +10,7 @@ import FindIdAndPwd from './pages/findIdAndPwd';
 import { Container } from './pages/Login/Container'
 import { Signup } from './pages/signup';
 import GameSearch from './pages/Home/homeGameSearch'
+import NewReview from './pages/newReview'
 
 
 class App extends React.Component {
@@ -63,25 +64,11 @@ class App extends React.Component {
 
       </Switch>
       </div>
-
       <Footer/>
       </BrowserRouter>
     )
   }
 }
 
-// const App=() =>{
-//   const [games, setGames] = useState([
-//     {"title":"Game #1", "genre" : "sports", "img":"https://cdn.shopify.com/s/files/1/0969/9128/products/Joker_-_Joaquin_Phoenix_-_Hollywood_Action_Movie_Poster_08339151-d79a-4b7b-8bc7-dcad04881c2c.jpg?v=1573629460"},
-//     {"title":"Game #2", "genre" : "action", "img":"https://cdn.shopify.com/s/files/1/1416/8662/products/titanic_1997_styleA_original_film_art_d26e81c0-1b87-4076-9da4-9fcdc0389ea5_1200x.jpg?v=1607475298"},
-//     {"title":"Game #3", "genre" : "sports", "img": "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/adventure-movie-poster-template-design-7b13ea2ab6f64c1ec9e1bb473f345547_screen.jpg?ts=1576732289"},
-// ])
-
-// return ( 
-//     <div>
-//       <GameList games={games}/>
-//     </div>
-//   )
-// }
 
 export default App;
---
+++ gamecrawler-client/src/pages/Home/top5Gmaes.js
@@ -0,0 +1,27 @@
+// import React, {useState, useEffect} from 'react';
+// import axios from 'axios';
+
+// function Top5Games(){
+//     const [top5, setTop5] = useState([]);
+
+//     useEffect( ()=>{
+//         axios
+//         .get('http://ec2-18-189-171-239.us-east-2.compute.amazonaws.com:5000/statistics/top5game',
+//         {withCredentials:true}
+//         ).then(res => {
+//             console.log(res.data);
+//             setTop5(res.data);
+        
+//         }).catch(err => alert(err))
+//     }, [])
+//     return (
+//         <div>
+//             <ul>
+//                 {
+//                     top5.map(top5 => <li key={top5.game_code}>{game_code}</li>)     //나중에 gmae poster로 변경하기
+//                 }
+//             </ul>
+//         </div>
+//     )
+// }
+// export default Top5Games;
