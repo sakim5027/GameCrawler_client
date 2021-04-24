b5bfde09752ae30d86d3716a2d6476c3e3e3b3f6
Seul An Kim <sakim5027@gmail.com>
2021-04-24 11:54:30 -0500

    working on build

--- gamecrawler-client/src/pages/Home/top5Gmaes.js
+++ gamecrawler-client/src/pages/Home/top5Gmaes.js
@@ -18,7 +18,7 @@ function Top5Games(){
         <div>
             <ul>
                 {
-                    top5.map(top5 => <li key={top5.game_code}>{game_code}</li>)     //나중에 gmae poster로 변경하기
+                   // top5.map(top5 => <li >{game_code}</li>)     //나중에 game poster로 변경하기
                 }
             </ul>
         </div>
