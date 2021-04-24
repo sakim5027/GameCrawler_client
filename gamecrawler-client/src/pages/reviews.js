9fcbda0ea9740effaf9c6583084613c8c12b46c3
Seul An Kim <sakim5027@gmail.com>
2021-04-24 18:51:56 -0500

    working on review page

--- gamecrawler-client/src/pages/reviews.js
+++ gamecrawler-client/src/pages/reviews.js
@@ -1,5 +1,15 @@
-export default function Reviews(){
-    return (
-        <div>review page</div>
-    )
+import React from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
+
+
+
+function Reviews(props) {
+  
+  return (
+    <div className="reviewList">
+    </div>
+  );
 }
+
+export default Reviews;
